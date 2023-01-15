1824ddcaf71d78a07d3237006fe0a4775a68821e
James Storm <jamesstorm@gmail.com>
2023-01-15 09:00:05 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+USERNAME=pi
+
 # the usual update and upgrade
 sudo apt update
 sudo apt upgrade -y
@@ -16,11 +18,19 @@ sudo apt install snapd
 echo "REMINDER!!! You might need to reboot and run again because snap"
 sudo snap install core
 
-
-
 #Neovim
-sudo snap install --beta nvim --classic
+sudo snap install nvim --edge --classic
 echo "REMINDER!!! You need to add /snap/bin to path at the top of .zshrc"
+
+#Github CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh -y
+gh auth login
+gh repo clone jamesstorm/prime /home/$USERNAME/prime
+
 
 #Screenfetch
 sudo apt install screenfetch
